c241b24c0e6fa2cd2032aec9d64d670ddba9df77
Victor Grishchenko <victor.grishchenko@gmail.com>
2016-06-13 19:51:43 +0500

    logical stamps

---
+++ include/stamp.h
@@ -0,0 +1,208 @@
+//
+// Created by gritzko on 6/13/16.
+//
+
+#ifndef SWARMCPP_STAMP_H
+#define SWARMCPP_STAMP_H
+
+#include <cstring>
+#include "64x64.h"
+
+// TODO
+// [ ] size_t for scan/print
+// [ ] scan/print methods of scan_t
+
+namespace swarm {
+
+    struct stamp_t {
+        base_t  time;
+        base_t  origin;
+
+        stamp_t () : time(), origin() {}
+        stamp_t (const stamp_t& copy) : time (copy.time), origin(copy.origin) {}
+
+        stamp_t (const char* stamp) : time(), origin() {
+            scan_t state;
+            scan(state, stamp, (int)strlen(stamp));
+        }
+        stamp_t (std::string stamp) {
+            scan_t state;
+            scan(state, stamp.c_str(), (int)stamp.length());
+        }
+
+        stamp_t (tm datetime, base_t orig) : origin(orig) {
+            int year = datetime.tm_year - 110;
+            if (year<0) {
+                time = base_t::INCORRECT;
+                return;
+            }
+            int month = year*12 + datetime.tm_mon;
+            uint64_t t = (uint64_t)month;
+            t <<= 12;
+            t |= datetime.tm_mday-1;
+            t <<= 6;
+            t |= datetime.tm_hour;
+            t <<= 6;
+            t |= datetime.tm_min;
+            t <<= 6;
+            t |= datetime.tm_sec;
+            t <<= 4*6; // msec, seq
+            time = t;
+        }
+
+        bool operator == (const stamp_t& b) const {
+            return time==b.time && origin==b.origin;
+        }
+        bool operator < (const stamp_t& b) const {
+            return time<b.time || (time==b.time && origin<b.origin);
+        }
+        bool operator > (const stamp_t& b) const {
+            return time>b.time || (time==b.time && origin>b.origin);
+        }
+        bool operator <= (const stamp_t& b) const {
+            return time<b.time || (time==b.time && origin<=b.origin);
+        }
+        bool operator >= (const stamp_t& b) const {
+            return time>b.time || (time==b.time && origin>=b.origin);
+        }
+
+        operator std::string () const {
+            char buf[22];
+            print_t state;
+            int length = print(state, buf, 22);
+            return std::string(buf, length);
+        }
+
+        bool isTranscendent () const {
+            return origin.isZero() || origin.isAbnormal();
+        }
+        bool isAbnormal () const {
+            return time.isAbnormal();
+        }
+        bool isError () const {
+            return time==base_t::INCORRECT;
+        }
+        bool isZero () const {
+            return time.isZero();
+        }
+        bool isNever () const {
+            return time.isInfinity();
+        }
+
+        tm datetime () const {
+            tm ret;
+            bzero(&ret, sizeof(ret));
+            uint64_t t = time.value;
+            t >>= 4*6; // ms, seq
+            int mask = 63;
+            ret.tm_sec = int(t)&mask;
+            t >>= 6;
+            ret.tm_min = int(t)&mask;
+            t >>= 6;
+            ret.tm_hour = int(t)&mask;
+            t >>= 6;
+            ret.tm_mday = 1 + (int(t)&mask);
+            t >>= 6;
+            int months = int(t);
+            ret.tm_mon = months%12;
+            ret.tm_year = (months/12) + 110;
+            // FIXME normalize
+            return ret;
+        }
+
+        bool isParentOf (const stamp_t& child) const {
+            if (isTranscendent()) return false;
+            uint64_t a=origin.value, b=child.origin.value;
+            while ( (a&63)==0 ) {
+                a >>= 6;
+                b >>= 6;
+            }
+            return a==b;
+        }
+
+        struct scan_t {
+            scan_t () : stage(0), state() {}
+            int stage;
+            base_t::scan_t state;
+        };
+        struct print_t {
+            print_t () : stage(0), state() {}
+            int stage;
+            base_t::print_t state;
+        };
+
+        int scan(scan_t &state, const char *buf, int length);
+        int print(print_t &state, char *buf, int length) const;
+
+    };
+
+    int stamp_t::scan(stamp_t::scan_t &state, const char *buf, int length) {
+        int offset = 0;
+        if (state.stage==0) {
+            offset = time.scan(state.state, buf, length);
+            if (offset==length) { // FIXME invalidate on -1
+                return offset;
+            } else if (offset<0) {
+                return offset;
+            }
+            state.state = base_t::scan_t();
+            state.stage = 1;
+        }
+        if (state.stage==1) {
+            if (buf[offset]!='+') {
+                origin = base_t();
+                return offset;
+            }
+            offset++;
+            state.stage = 2;
+        }
+        if (state.stage==2) { // FIXME 0 terminator
+            offset += origin.scan(state.state, buf+offset, length-offset);
+            if (offset<0) { // FIXME  -1
+                return offset;
+            } else if (offset==length) {
+                return offset;
+            }
+            state.stage = 0;
+            state.state = base_t::scan_t();
+        }
+        return offset;
+    }
+
+    int stamp_t::print(stamp_t::print_t &state, char *buf, int length) const {
+        int offset = 0;
+        if (state.stage==0) {
+            offset = time.print(state.state, buf, length);
+            if (offset<0) {
+                return offset;
+            } else if (offset==length) {
+                return offset;
+            }
+            state.state = base_t::print_t();
+            state.stage = 1;
+        }
+        if (state.stage==1) {
+            if (isTranscendent()) {
+                state.stage = 0;
+                state.state = base_t::print_t();
+                return offset;
+            }
+            buf[offset++] = '+';
+            state.stage = 2;
+        }
+        if (state.stage==2) {
+            offset += origin.print(state.state, buf+offset, length-offset);
+            if (offset<0) {
+                return offset;
+            } else if (offset==length) {
+                return offset;
+            }
+            state.state = base_t::print_t();
+            state.stage = 0;
+        }
+        return offset;
+    }
+
+}
+
+#endif //SWARMCPP_STAMP_H
---
+++ test/stamp.cpp
@@ -0,0 +1,116 @@
+//
+// Created by gritzko on 6/13/16.
+//
+#include "../include/stamp.h"
+#include <assert.h>
+#include <ctime>
+
+using namespace swarm;
+using namespace std;
+
+int main (int argn, char** args) {
+
+    char buf[1024];
+    stamp_t::print_t state;
+    // zero (is-zero: true), named zero
+    const char* str0 = "0";
+    stamp_t zero(str0);
+    assert(zero.isZero());
+    assert(zero.isTranscendent());
+    assert(!zero.isAbnormal());
+    assert(!zero.isError());
+    printf("%s\n", string(zero).c_str());
+
+    const char* str0my = "0+my";
+    stamp_t myzero(str0my);
+    assert(!myzero.isTranscendent());
+    assert(myzero.isZero());
+    printf("%s\n", string(myzero).c_str());
+
+    const char* str_trans = "transcndnt";
+    stamp_t trans(str_trans);
+    assert(trans.isTranscendent());
+    assert(!trans.isZero());
+    printf("%s\n", string(trans).c_str());
+
+    // never, named never
+    const char* str_never = "~";
+    stamp_t never(str_never);
+    assert(never.isTranscendent());
+    assert(!never.isZero());
+    assert(never.isNever());
+    printf("%s\n", string(never).c_str());
+
+    const char* str_nnever = "~+my";
+    stamp_t nnever(str_nnever);
+    assert(!nnever.isTranscendent());
+    assert(!nnever.isZero());
+    assert(nnever.isNever());
+    printf("%s\n", string(nnever).c_str());
+
+    // parent-of true
+    const char* str_parent = "1CQAneD+parnt";
+    const char* str_child = "1CQAnek+parntsCHILD";
+    stamp_t parent(str_parent);
+    stamp_t child(str_child);
+    assert(parent.isParentOf(child));
+    printf("%s\n%s\n", string(parent).c_str(), string(child).c_str());
+
+    // 1 Jan 2010 00:00:01 UTC
+    const char* str_epoch1sec = "000001+my";
+    stamp_t epoch1sec(str_epoch1sec);
+    tm time = epoch1sec.datetime();
+    assert(time.tm_year==110);
+    assert(time.tm_mon==0);
+    assert(time.tm_mday==1);
+    assert(time.tm_hour==0);
+    assert(time.tm_min==0);
+    assert(time.tm_sec==1);
+    printf("%s\n", string(epoch1sec).c_str());
+
+    // Fri May 27 20:50:00 UTC 2016: scan and print both formats
+    tm ser;
+    bzero(&ser, sizeof(ser));
+    ser.tm_year = 116;
+    ser.tm_mon = 4;
+    ser.tm_mday = 27;
+    ser.tm_hour = 20;
+    ser.tm_min = 50;
+    stamp_t stamp_ser(ser, "my");
+    char str_ser[1024];
+    setlocale(LC_TIME, "en_EN.utf8");
+    strftime(str_ser, 1024, "%a %b %d %H:%M:%S %Z %Y", &ser);
+    printf("%s\n%s\n", string(stamp_ser).c_str(), str_ser);
+
+    // parse 1CQAn00000+my: the same timestamp untrimmed
+    const char* str_pt = "1CQAn00000+my";
+    stamp_t pt(str_pt);
+    tm time_pt = pt.datetime();
+    strftime(str_ser, 1024, "%a %b %d %H:%M:%S %Z %Y", &time_pt);
+    printf("%s\n", str_ser);
+
+    // is-abnormal: true (not a timestamp)
+    stamp_t abnormal("~abc+author");
+    assert(abnormal.isAbnormal());
+    printf("%s\n", string(abnormal).c_str());
+
+    // is-transcendent: true (the right half is not an origin)
+    stamp_t trans2 ("Object+~3");
+    assert(trans2.isTranscendent());
+    printf("%s\n", string(trans2).c_str());
+
+    // parse Object+0: zero origin is skipped, transcendent: true
+    stamp_t object ("Object+0");
+    assert(object.isTranscendent());
+    printf("%s\n", string(object).c_str());
+
+    // is-error: true, is-transcendent: false, is-abnormal: true, is-zero: false
+    stamp_t error("~~~~~~~~~~+origin");
+    assert(error.isError());
+    assert(!error.isTranscendent());
+    assert(error.isAbnormal());
+    assert(!error.isZero());
+
+    return 0;
+
+}
