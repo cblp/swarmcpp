d8f9c18eade4c4fef1e3e6d346f81e09405e93d4
Victor Grishchenko <victor.grishchenko@gmail.com>
2016-07-20 13:49:56 +0300

    test script fixes

--- test-by-spec.sh
+++ test-by-spec.sh
@@ -3,6 +3,7 @@
 TESTDIR="swarm-protocol-docs/test"
 if [ ! -e $TESTDIR ]; then
     git submodule init
+    git submodule update --init --recursive
 fi
 cd swarm-protocol-docs
 #git checkout master
@@ -13,26 +14,32 @@ cd -
 rm -rf tmp
 mkdir tmp
 
-make || exit -1
+if [ ! -e Makefile ]; then
+    cmake CMakeLists.txt || exit 1
+fi
+
+make || exit 2
 echo
 
 grep -v '^;' $TESTDIR/64x64.batt > tmp/64x64.ok
 ./test64x64 > tmp/64x64.out
-diff -BU3 tmp/64x64.out tmp/64x64.ok || exit -2
+diff -BU3 tmp/64x64.out tmp/64x64.ok || exit 3
 echo OK 64x64
 
 grep -v '^;' $TESTDIR/stamp.batt | grep -v '^$' > tmp/stamp.ok
 ./teststamp > tmp/stamp.out
-diff -BU3 tmp/stamp.out tmp/stamp.ok || exit -2
+diff -BU3 tmp/stamp.out tmp/stamp.ok || exit 4
 echo OK stamp
 
 grep -v '^;' $TESTDIR/spec.batt | grep -v '^$'  > tmp/spec.ok
 ./testspec > tmp/spec.out
-diff -BU3 tmp/spec.out tmp/spec.ok || exit -2
+diff -BU3 tmp/spec.out tmp/spec.ok || exit 5
 echo OK spec
 
 grep -v '^;' $TESTDIR/op.batt | grep -v '^$'  > tmp/op.ok
 ./testop > tmp/op.out
-diff -BU3 tmp/op.out tmp/op.ok || exit -2
+diff -BU3 tmp/op.out tmp/op.ok || exit 6
 echo OK op
+
+echo DONE
 
